e0784037060a38a97c6663f2c6d607adac3a2646
Javier Ovelar <isaiasjovelar@gmail.com>
2025-03-23 17:30:46 -0300

    .

---
+++ Trabajo_practico_1_Ovelar_Isaias_Javier.py
@@ -0,0 +1,24 @@
+def a1():
+    print("hola mundo!")
+
+def a2():
+    nombre=input("ingrese su nombre: ")
+    print(f"hola {nombre}")
+
+def a3():
+    nombre = input("Ingrese su nombre: ")
+    apellido = input("ingrese su apellido: ")
+    edad = input("Ingrese su edad: ")
+    residencia = input("Ingrese s lugar de residencia")
+    print(f"Hola soy {apellido} {nombre} tengo {edad} anios y vivo en {residencia}")
+
+def a4():
+    radio = input("Ingrese el radio del circulo: ")
+    print(f"El area de un circulo con radio {radio} es de {3.14*(float(radio)**2)} y su perimetro es de {2*3.14*float(radio)}")
+
+def a5():
+    tiempo = input("Ingrese el tiempo en segundos: ")
+    print(f"El tiempo ingresado equivale a {float(tiempo)/3600} horas ")
+
+def a6():
+    
